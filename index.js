d0b2a20a4d9ab518b487307d44cd0e1c3203418f
Leandro Augusto <leandroaugusto470@gmail.com>
2023-08-25 16:28:34 -0300

    express url encoded adicionado

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
