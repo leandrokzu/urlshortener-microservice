d6dfcb16acfca6b5a1407699d7079ae03e7a6e01
Leandro Augusto <leandroaugusto470@gmail.com>
2023-08-25 16:23:39 -0300

    url shortener e redirect configurados

--- index.js
+++ index.js
@@ -10,15 +10,43 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+const originalUrls = [];
+const shortUrls = [];
+
+app.post('/api/shorturl', function (req, res) {
+  const { url } = req.body;
+  const urlRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+
+  if (!urlRegex.test(url)) {
+    res.json({ error: 'invalid url' });
+  }
+  if (originalUrls.includes(url)) {
+    res.json({
+      original_url: url,
+      short_url: originalUrls.indexOf(url),
+    });
+  }
+  originalUrls.push(url);
+  shortUrls.push(shortUrls.length);
+
+  res.json({ original_url: url, short_url: shortUrls.length - 1 });
+});
+
+app.get('/api/shorturl/:index?', function (req, res) {
+  const { index } = req.params;
+
+  res.status(301).redirect(originalUrls[index]);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
