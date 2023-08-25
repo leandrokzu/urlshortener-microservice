ae1ce5506a251d2d01f278ea23b98707d07adee2
Leandro Augusto <leandroaugusto470@gmail.com>
2023-08-25 18:02:38 -0300

    ajustando validação

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ const shortUrls = [];
 
 app.post('/api/shorturl', function (req, res) {
   const { url } = req.body;
-  // const urlRegex =
-  //   /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/i;
+  const urlRegex =
+    /^(https?:\/\/)(www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}([/a-zA-Z0-9 .-]*)*\/?$/;
 
-  // if (!urlRegex.test(url)) {
-  //   res.json({ error: 'invalid url' });
-  // }
+  if (!urlRegex.test(url)) {
+    res.json({ error: 'invalid url' });
+  }
   if (originalUrls.includes(url)) {
     res.json({
       original_url: url,
